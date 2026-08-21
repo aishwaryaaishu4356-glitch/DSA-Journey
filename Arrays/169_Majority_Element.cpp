7eb4325bdbe8dbc9f22e0ab036d880c76e72a945
Aishwarya B <aishwaryaaishu4356@gmail.com>
2026-08-20 19:31:01 -0500

    Solved Leetcode 169 Majority Element

---
+++ Arrays/169_Majority_Element.cpp
@@ -0,0 +1,33 @@
+/*
+Leetcode 169: Majority Element
+Approach: Brute force optimised with sorting
+Time Complexity: 
+Space Complexity:
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int majorityElement(vector<int>& nums) {
+      int n=nums.size();
+      sort(nums.begin(),nums.end());
+      int freq=1,ans=nums[0];
+      for(int i=1;i<n;i++){
+        if(nums[i]==nums[i-1]){
+            freq++;
+        }else{
+            freq=1;
+            ans=nums[i];
+        }
+        if(freq>n/2){
+            return ans;
+        }
+      } 
+    return ans; 
+    }
+int main(){
+    vector<int> vec={1,2,1};
+    int result=majorityElement(vec);
+    cout<<result<<endl;
+    return 0;
+}
